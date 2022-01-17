36bc1148e3379b057660ef8c00a8265e86c5419a
某星 <51886709+ZxEnjoy@users.noreply.github.com>
2022-01-17 18:38:20 +0800

    init
    wish happy game every day

---
+++ Image-Upload/src/compoents/files-input/FilesInput.js
@@ -0,0 +1,45 @@
+import './index.css'
+import { useRef ,useEffect} from 'react'
+const FilesInput = ()=>{
+    const filds = useRef()
+    const handleChange=(file)=>{
+        const files = file.target.files
+        console.log(file.target.files)    
+    }
+    useEffect(
+        ()=>{
+            filds.current.webkitdirectory=true
+        },
+        [filds]
+    )
+    return(
+        <div className='files-input'>
+            <input
+                ref={filds} 
+                className='files-input-files'
+                type='file' 
+                onChange={handleChange}
+                onDragOver={(e)=>{
+                    //console.log(e)
+                    if(e.dataTransfer.items[0].type!==''){
+                        filds.current.webkitdirectory=false
+                    }else{
+                        filds.current.webkitdirectory=true
+                    }
+                }}
+                onMouseEnter={()=>{
+                    filds.current.webkitdirectory=true
+                }}
+                multiple='multiple' 
+                ></input>
+        </div>
+    )
+}
+export default FilesInput
+// dataTransfer: DataTransfer
+// dropEffect: "none"
+// effectAllowed: "all"
+// files: FileList {length: 0}
+// items: DataTransferItemList {length: 0}
+// types: Array(0)
+// length: 0
